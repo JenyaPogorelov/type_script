75567d95a1f557b654b409dcfcf9cd38f2aa086f
ShadowD <jeckcame@gmail.com>
2022-06-04 23:45:33 +0300

    ДЗ2 Работа над 1 пунктом дз

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,15 @@ import {renderSearchStubBlock} from './search-results.js'
 import {renderUserBlock} from './user.js'
 import {renderToast} from './lib.js'
 import {renderSearchResult} from './search-results-button.js';
+import {getFavoritesAmount, getUserData} from "./get-user-info.js";
 
 window.addEventListener('DOMContentLoaded', () => {
   renderUserBlock('Wade Warren', '/img/avatar.png', '0');
   renderSearchFormBlock();
   renderSearchStubBlock();
   renderSearchResult();
+  getUserData();
+  getFavoritesAmount()
   // renderToast(
   //   {text: 'Это пример уведомления. Используйте его при необходимости', type: 'success'},
   //   {name: 'Понял', handler: () => {console.log('Уведомление закрыто')}}
