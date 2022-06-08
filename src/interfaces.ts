c0509ea8a44b612928536156504d3d8e12da49a4
ShadowD <jeckcame@gmail.com>
2022-06-08 17:17:31 +0300

    ДЗ2 полностью закончит ДЗ

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -11,5 +11,11 @@ export interface SearchFormData {
 }
 
 export interface Place {
-
+  id: number;
+  image: string;
+  name: string;
+  description: string;
+  remoteness: number;
+  bookedDates: number[];
+  price: number;
 }
